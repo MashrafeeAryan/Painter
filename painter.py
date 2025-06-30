b6281bfc3b6f0a0a8a56406cb06e572caa882dc7
Mashrfee Aryan <aryanmashrafee07@gmail.com>
2025-06-30 16:51:45 -0500

    Second Fix

--- painter.py
+++ painter.py
@@ -7,6 +7,8 @@ import cv2
 import mediapipe as mp
 video = cv2.VideoCapture(0)
 
+is_drawing = False
+
 hands = mp.solutions.hands.Hands()
 canvas = np.ones((480, 640, 3), dtype=np.uint8) * 255
 cv2.imshow("Canvas", canvas)
@@ -19,11 +21,20 @@ while True:
     if landmark.multi_hand_landmarks:
         for hand in landmark.multi_hand_landmarks:
             index_tip = hand.landmark[8]
-            x = int(index_tip.x * 640)
-            y = int(index_tip.y * 480)
-            prev_x, prev_y = x, y
-            if prev_x is not None and prev_y is not None:
-                cv2.line(canvas, (prev_x, prev_y), (x, y), (0,255,0), thickness=3)
+            thumb_tip = hand.landmark[4]
+            
+            thumb_x = int(thumb_tip.x * 640)
+            thumb_y = int(thumb_tip.y * 480)
+            
+            index_x = int(index_tip.x * 640)
+            index_y = int(index_tip.y * 480)
+            
+            if (index_y - thumb_y) < 5:                
+                prev_x, prev_y = index_x, index_y
+                
+                if prev_x is not None and prev_y is not None:
+                    cv2.line(canvas, (prev_x, prev_y), (index_x, index_y), (0,255,0), thickness=3)
+            
     else:
         print("No Hands")
         prev_x, prev_y = None, None
